30fe1c74a3eec70aa231f2c2d7bc67c6cc533341
Anastasia Lunina <lunina.anastasiya@gmail.com>
2022-10-11 01:22:55 -0400

    Add individual listings

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
@@ -19,7 +19,12 @@ def index(request):
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing,
+    }
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
